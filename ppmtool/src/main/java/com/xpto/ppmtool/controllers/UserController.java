e0c7741d1de4c633d10e0c400d434ce309739b7f
henriquedeof <henriquedeof@gmail.com>
2020-08-03 19:09:08 +1000

    Generating jwt token.

--- ppmtool/src/main/java/com/xpto/ppmtool/controllers/UserController.java
+++ ppmtool/src/main/java/com/xpto/ppmtool/controllers/UserController.java
@@ -1,11 +1,19 @@
 package com.xpto.ppmtool.controllers;
 
 import com.xpto.ppmtool.domain.User;
+import com.xpto.ppmtool.payload.JWTLoginSuccessResponse;
+import com.xpto.ppmtool.payload.LoginRequest;
+import com.xpto.ppmtool.security.JwtTokenProvider;
+import com.xpto.ppmtool.security.SecurityConstants;
 import com.xpto.ppmtool.services.UserService;
 import com.xpto.ppmtool.utils.validations.MapValidationError;
 import com.xpto.ppmtool.validator.UserValidator;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,13 +26,36 @@ import javax.validation.Valid;
 @RequestMapping("/api/users")
 public class UserController {
 
-    private UserService userService;
-    private UserValidator userValidator;
+    private final UserService userService;
+    private final UserValidator userValidator;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final AuthenticationManager authenticationManager;
 
-    public UserController(UserService userService, UserValidator userValidator) {
+    public UserController(UserService userService, UserValidator userValidator, JwtTokenProvider jwtTokenProvider, AuthenticationManager authenticationManager) {
         this.userService = userService;
         this.userValidator = userValidator;
+        this.jwtTokenProvider = jwtTokenProvider;
+        this.authenticationManager = authenticationManager;
     }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest, BindingResult bindingResult){
+        ResponseEntity<?> errorMap = MapValidationError.getMapValidation(bindingResult);//validating Inputs of project param.
+        if (errorMap != null){
+            return errorMap;
+        }
+
+        Authentication authentication = this.authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword())
+        );
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = SecurityConstants.TOKEN_PREFIX + this.jwtTokenProvider.generateToken(authentication);
+
+        return ResponseEntity.ok(new JWTLoginSuccessResponse(true, jwt));
+
+    }
+
 
     @PostMapping("/register")
     public ResponseEntity<?> registerUser(@Valid @RequestBody User user, BindingResult bindingResult){
---
+++ ppmtool/src/main/java/com/xpto/ppmtool/security/JwtTokenProvider.java
@@ -0,0 +1,39 @@
+package com.xpto.ppmtool.security;
+
+import com.xpto.ppmtool.domain.User;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class JwtTokenProvider {
+
+
+    //Generating a token when I have a good username and password
+    public String generateToken(Authentication authentication){
+        User user = (User) authentication.getPrincipal();
+        Date now = new java.util.Date(System.currentTimeMillis());
+
+        Date expiryDate = new Date(now.getTime() + SecurityConstants.EXPIRATION_TIME);
+        String userId = Long.toString(user.getId());
+
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("id", Long.toString(user.getId()));
+        claims.put("username", user.getUsername());
+        claims.put("fullName", user.getFullName());
+
+        return Jwts.builder()
+                .setSubject(userId)
+                .setClaims(claims) //claims is information about the user
+                .setIssuedAt(now)
+                .setExpiration(expiryDate)
+                .signWith(SignatureAlgorithm.HS512, SecurityConstants.SECRET)
+                .compact();
+    }
+
+}
