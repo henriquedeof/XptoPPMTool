afba19dce8b42857236cb76672fbd6a992417074
henriquedeof <henriquedeof@gmail.com>
2020-08-10 18:54:08 +1000

    Changing token timeout to 400000

--- ppmtool/src/main/java/com/xpto/ppmtool/security/SecurityConstants.java
+++ ppmtool/src/main/java/com/xpto/ppmtool/security/SecurityConstants.java
@@ -7,6 +7,6 @@ public class SecurityConstants {
     public static final String SECRET = "SecretKeyToGenJWTs";
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
-    public static final long EXPIRATION_TIME = 300_000;//30s or 30000ms
+    public static final long EXPIRATION_TIME = 400000;//30s or 30000ms
 
 }
